af628cb76ae1db54b62aee3bda31c3e81e8cbfaa
Wedrig <jackkmorice@gmail.com>
2017-04-05 10:08:12 -0500

    Delete page.js

--- page.js
+++
@@ -1,3 +0,0 @@
-document.body.innerHTML='<div class="nav"><a href="//badbacco.github.io"><li>Badbacco</li></a><a href="//badbacco.github.io/wit.html"><li>What is Tobacco?</li></a></div> \
-<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//badbacco.github.io/theme.css'
-
